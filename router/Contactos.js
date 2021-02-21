5c3c5e97711213254ff61661adc49eafc615f9e0
EMarines <61839920+EMarines@users.noreply.github.com>
2021-02-21 12:22:54 -0700

    proyecto funional

--- router/Contactos.js
+++ router/Contactos.js
@@ -16,4 +16,85 @@ router.get('/', async (req, res) => {
     console.log(error)
   }
 })
+
+router.get('/crear', (req, res) => {
+  res.render('crear')
+})
+
+
+router.post('/', async (req, res) => {
+  const body = req.body
+  console.log(body)
+
+  try {
+    await Contacto.create(body)
+    res.redirect('/contactos')
+
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+router.get('/:id', async(req, res) =>{
+  const id = req.params.id
+  try {
+    const contactoDB = await Contacto.findOne({ _id: id})
+    console.log(contactoDB)
+
+    res.render('detalle', {
+      contacto: contactoDB,
+      error: false
+    })
+  } catch (error) {
+    console.log(error) 
+    res.render('detalle', {
+      error: true,
+      mensaje: 'No se encontró al Contacto'  
+    })
+  }
+})
+
+router.delete('/:id', async(req, res) =>{
+  const id = req.params.id
+
+  try {
+    const contactoDB = await Contacto.findByIdAndDelete({ _id: id})
+
+    if (contactoDB) {
+      res.json({
+        estado: true,
+        mensaje:'eliminado!'
+      })
+    }else {
+      res.json({
+        estado: false,
+        mensaje:'falló eliminar!'
+      })
+    }
+
+  } catch (error) {
+    console.log(error)
+  }
+} )
+
+router.put('/:id', async(req, res) => {
+  const id = req.params.id
+  const body = req.body
+
+  try {
+    const contactoDB = await Contacto.findByIdAndUpdate(id, body, { useFindAndModify: false })
+    console.log(contactoDB)
+
+    res.json({
+      estado: true,
+      mendaje: 'Editado'
+    })
+  } catch (error) {
+    console.log(error)
+    res.json({
+      estado: false,
+      mendaje: 'No Se Pudo Editar!'
+    })
+  }
+})
 module.exports = router;
